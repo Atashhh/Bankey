9d3a83a72c943e5289479147dc1e6b34c246f225
Atash Musazada <a.musazada@gmail.com>
2023-06-14 09:46:50 +0400

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Atash Musazada on 14.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
