a172a978808b48833bae4def5e479598d5b7bf1a
Atashhh <atash.musazade@gmail.com>
2023-06-15 15:58:06 +0400

    New ASAN Login Page

--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    let headerImage = UIImageView()
+    let heroImage = UIImageView()
+    let firstLabel = UILabel()
+    let secondLabel = UILabel()
     let loginView = LoginView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +26,70 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController {
     private func style() {
-        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let secondLabeltext = "Fərdi məlumatlarınızı rəqəmsal qaydada əldə etmək bir klik qədər yaxındır!"
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 10
+        let attributedText = NSAttributedString(string: secondLabeltext, attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+                
+        
+        view.backgroundColor = UIColor(named: "login-background-color")
+        
+        headerImage.translatesAutoresizingMaskIntoConstraints = false
+        headerImage.image = UIImage(named: "header-img")
+        
+        heroImage.translatesAutoresizingMaskIntoConstraints = false
+        heroImage.image = UIImage(named: "hero-img")
+        
+        firstLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstLabel.text = "Sizin rəqəmsal portfeliniz!"
+        firstLabel.textColor = UIColor(named: "title-color")
+        firstLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        
+        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+        secondLabel.text = secondLabeltext
+        secondLabel.textColor = UIColor(named: "title-color")
+        secondLabel.numberOfLines = 0
+        secondLabel.lineBreakMode = .byWordWrapping
+        secondLabel.attributedText = attributedText
+        secondLabel.textAlignment = .center
+        secondLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        
+        
+        
     }
         
     private func layout() {
-        view.addSubview(loginView)
+        view.addSubview(headerImage)
+        view.addSubview(heroImage)
+        view.addSubview(firstLabel)
+        view.addSubview(secondLabel)
         
+        //HEADER
         NSLayoutConstraint.activate([
-            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+            headerImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            headerImage.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 6)
         ])
+        
+        //HERO
+        NSLayoutConstraint.activate([
+            heroImage.topAnchor.constraint(equalToSystemSpacingBelow: headerImage.bottomAnchor, multiplier: 6),
+            heroImage.centerXAnchor.constraint(equalTo: headerImage.centerXAnchor)
+        ])
+        
+        //FIRSTLABEL
+        NSLayoutConstraint.activate([
+            firstLabel.topAnchor.constraint(equalToSystemSpacingBelow: heroImage.bottomAnchor, multiplier: 4.3),
+            firstLabel.centerXAnchor.constraint(equalTo: heroImage.centerXAnchor)
+        ])
+        
+        //SECONDLABEL
+        NSLayoutConstraint.activate([
+            secondLabel.topAnchor.constraint(equalToSystemSpacingBelow: firstLabel.bottomAnchor, multiplier: 2.5),
+            secondLabel.centerXAnchor.constraint(equalTo: firstLabel.centerXAnchor),
+            secondLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 9)
+        ])
+        
     }
 }
     
